bd05887ba4b39f483962ecf77b169a47ad9b0ed5
yigitkaraduman <yigitkaraduman@gmail.com>
2020-11-29 23:57:02 +0300

    Set the port number from environment variable PORT

--- src/main/java/myDemoApp/App.java
+++ src/main/java/myDemoApp/App.java
@@ -7,12 +7,15 @@ import static spark.Spark.get;
 import static spark.Spark.port;
 import static spark.Spark.post;
 
+import java.lang.System.Logger;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import spark.ModelAndView;
 import spark.template.mustache.MustacheTemplateEngine;
+import static spark.Spark.port;
+
 
 
 import java.util.ArrayList;
@@ -84,6 +87,13 @@ public class App {
               return new ModelAndView(map, "compute.mustache");
             },
             new MustacheTemplateEngine());
+
+        /* Logger logger = LogManager.getLogger(App.class);
+
+        int port = Integer.parseInt(System.getenv("PORT"));
+        port(port);
+        ((Object) logger).error("Current port number:" + port);
+            */
     }
 
     static int getHerokuAssignedPort() {
